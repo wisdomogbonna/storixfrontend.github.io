50571ca645970f7e3d7b990435cc81287f064cb1
wisdomogbonna <chinwenduogbonna157@gmail.com>
2025-10-21 18:07:43 +0100

    updated

---
+++ src/pages/Contact.js
@@ -0,0 +1,10 @@
+import React from "react";
+
+export default function Contact() {
+  return (
+        <div style={{ padding: "30px" }}>
+                    <h2>📩 Contact Us</h2>
+                                      <p>Email: Storixentertainmentcasting@gmail.com</p>
+                                                            </div>
+                                                                                    );
+                                                                                                            }
