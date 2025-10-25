6b24d9e24ddf33a16e55b5e48c8ea2fd880b7869
wisdomogbonna <chinwenduogbonna157@gmail.com>
2025-10-25 11:39:57 +0100

    updated

--- src/components/ProtectedRoute.js
+++ src/components/ProtectedRoute.js
@@ -1,15 +1,15 @@
+// src/components/ProtectedRoute.js
 import React from "react";
 import { Navigate } from "react-router-dom";
 
 export default function ProtectedRoute({ children }) {
-  // Check if a user exists in localStorage (from login)
-    const user = JSON.parse(localStorage.getItem("user"));
+  const user = JSON.parse(localStorage.getItem("user"));
 
-      // If not logged in, send to /login
-        if (!user) {
-            return <Navigate to="/login" replace />;
-              }
+    // If no user is found, redirect to login
+      if (!user) {
+          return <Navigate to="/login" replace />;
+            }
 
-                // Otherwise, show the protected content (stories, etc.)
-                  return children;
-                  }6
+              // Otherwise, render the child components
+                return children;
+                }
